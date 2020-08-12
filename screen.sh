a915cee437f2ed059fa63fa3d6a19bd0c8c6f954
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2020-08-12 11:13:31 +0700

    Add files via upload

---
+++ screen.sh
@@ -0,0 +1,140 @@
+#!/ust/etc/bash
+#Author INDOnimous
+#Github : https://github.com/INDOnimous
+#Youtube : https://YouTube.com/OFFICIALINDOnimousOFFICIALI>
+#inatagram : https://Instagram.com/indonimous
+#Website : www.pasaronlen.com
+#
+#
+#
+#Color  Code
+B='\033[1;30m'  #Black
+R='\033[1;31m'  #Red
+P='\033[1;35m'  #Purple
+Br='\033[1;33m' #Brown
+Bl='\033[1;34m' #Blue
+G='\033[1;32m'  #Green
+C='\033[1;36m'  #Cyan
+W='\033[1;37m' #White
+M='\033[1;3m' #Miring
+J='\033[1;0m'
+#
+#                                                                                                                                             #
+#
+clear
+rm -rf /data/data/com.termux/files/home/Termux_Screen/
+echo -e "[•] Sedang Menganbil Data Anda.."
+sleep 5
+echo -n -e "[•] Selamat Datang Kembali Tuan "$R;whoami 
+sleep 3
+#
+#
+echo ""
+echo -e $G"===-===-===-===-===-===-===-===-==="
+echo -n -e $G"|| ";date |lolcat
+echo -e $G"===-===-===-===-===-===-===-===-==="
+sleep 4
+clear
+echo ""
+echo -e "\n\n\n\n\n\n\n\n\n\n"
+echo -e "\t\t\b%%%%%%%%%%%%%%%%%%%\n\t\t%% EROR GAGAL %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n"
+echo -e "\t\t\t%%%%%%%%%%%%%%%%%%%\n\t\t\t%%"$W" EROR GAGAL "$G"%%\n\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n"
+echo -e "%%%%%%%%%%%%%%%%%%%\n%%"$R" EROR GAGAL"$G" %%\n%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n"
+echo -e "\t\t\t\t%%%%%%%%%%%%%%%%%%%\n\t\t\t\t%%"$P" EROR GAGAL"$G" %%\n\t\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n\n"
+echo -e "\t\t %%%%%%%%%%%%%%%%%%%\n\t\t%%"$Br" EROR GAGAL"$G" %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n"
+echo -e "\t\t\t\t\t\t %%%%%%%%%%%%%%%%%%%\n\t\t\t\t\t\t%%"$Bl" EROR GAGAL"$G" %%\n\t\t\t\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e ""
+echo -e " %%%%%%%%%%%%%%%%%%%\n%%"$C" EROR GAGAL"$G" %%\n%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n\n\n"
+echo -e "\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t%%"$P" EROR GAGAL"$G" %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n"
+echo -e "\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t%%"$Br" EROR GAGAL"$G" %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n"
+echo -e "\t\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t\t%%"$Br" EROR GAGAL"$G" %%\n\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n\n\n"
+echo -e "\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t%%"$Bl" EROR GAGAL"$G" %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n"
+echo -e "\t\t\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t\t\t%%"$C" EROR GAGAL"$G" %%\n\t\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n"
+echo -e "\t\t\t %%%%%%%%%%%%%%%%%%%\n\t\t\t%%"$W" EROR GAGAL "$G"%%\n\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n\n\n"
+echo -e "\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t%%"$Br" EROR GAGAL"$G" %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n"
+echo -e "\t\t\t %%%%%%%%%%%%%%%%%%%\n\t\t\t%%"$Bl" EROR GAGAL"$G" %%\n\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n"
+echo -e " %%%%%%%%%%%%%%%%%%%\n%%"$C" EROR GAGAL"$G" %%\n%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n"
+echo -e "\t %%%%%%%%%%%%%%%%%%%\n\t%% EROR GAGAL %%\n\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n"
+echo -e "\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t%%"$W" EROR GAGAL "$G"%%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+clear
+echo -e "\n\n\n\n"
+echo -e "\t\t\t\t\t %%%%%%%%%%%%%%%%%%%\n\t\t\t\t\t%%"$C" EROR GAGAL"$G" %%\n\t\t\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e ""
+echo -e "\t\t\b %%%%%%%%%%%%%%%%%%%\n\t\t%%"$P" EROR GAGAL"$G" %%\n\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e "\n\n\n\n\n\n\n\n\n\n"
+echo -e "\t\t\t\t\t %%%%%%%%%%%%%%%%%%%\n\t\t\t\t\t%%"$Br" EROR GAGAL"$G" %%\n\t\t\t\t\t%%%%%%%%%%%%%%%%%%%"
+sleep 0.5
+clear
+echo -e $C"\t\t[~]"$G" WELCOME IN TERMINAL"$C" [~]"
+echo ""
+echo ""
+echo -e $C">>\t"$R"["$Bl" MAAF"$W" TUAN KESALAHAN TIDAK AKAN DI ULAGI LAGI "$R" ]"
+echo -e $C">>\t"$R"["$Bl" SELAMAT"$W "DATANG TUAN KU SEMOGA "$R"]"
+echo -e $C">>\t"$R"[ "$W"HARIMU "$Bl"MENYE"$W" NANGKAN "$R"]"
+echo -e $C">>\t"$R"["$W" TOOLS"$Bl" DI BUAT OLEH"$W" MR ANKER TUAN   "$R"]"
+echo -e $C">>\t"$R"["$W" JANGAN LUPAKAN "$Bl"TUAN ANKER TUAN           "$R"]"
+echo ""
+echo ""
+echo -e $P"\t  ["$Bl"&"$P"]"$W" Instan Screen By 4NK3R-EROR "$P"["$Bl"&"$P"]"
+echo ""
+echo -e ">>>\t\t"$G $M"[ WWW.PASARONLEN.COM ]"$J""
+echo""
+date |lolcat
+echo -e $G ""
+fish
